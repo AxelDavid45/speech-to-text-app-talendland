33995d572017e7930fc1ee2a220e44375a17bba8
Axel Espinosa <axeldavid45@gmail.com>
2020-07-28 01:13:06 -0500

    Added IBM service and authentication method added

--- configs/index.js
+++ configs/index.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 const config = {
   port: process.env.PORT,
   dev: process.env.MODE !== 'production',
+  speechApiKey: process.env.SPEECH_API_KEY,
+  speechUrl: process.env.SPEECH_URL
 };
 
 module.exports = config;
---
+++ services/speechToText.js
@@ -0,0 +1,16 @@
+const IbmSpeechToText = require('ibm-watson/speech-to-text/v1');
+const { IamAuthenticator } = require('ibm-watson/auth');
+const config = require('../configs/index');
+
+class SpeechToText {
+  autenticate () {
+    return new IbmSpeechToText({
+      authenticator: new IamAuthenticator({
+        apikey: config.speechApiKey
+      }),
+      url: config.speechUrl
+    });
+  }
+}
+
+module.exports = SpeechToText;
