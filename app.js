6b8316c6a0c25413c1e1dc226f7b0f5570aba093
Axel Espinosa <axeldavid45@gmail.com>
2020-12-21 17:24:48 -0600

    fix: cors added

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const app = express();
 const config = require('./configs/index');
 const routes = require('./routes/routes');
 const errorHandling = require('./utils/middleware/errorHandling');
+const cors = require('cors')
+
+app.use(cors())
 
 app.use(routes);
+
 // Middleware
 app.use(errorHandling);
 
